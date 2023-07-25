6af35818a1ff37e774940d0aafdbc637407272a0
Anis CHALI <anis.chali@exfo.com>
2023-07-25 01:22:16 -0400

    qbackend_test: add a test command line application to test the qbackend library.

---
+++ test/backend_test.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+#include <cstdlib>
+#include <json_engine.hpp>
+
+
+void *json_decode_callback (const char *json)
+{
+    return nullptr;
+}
+
+using namespace qbackend;
+
+
+int main(int argc, char **argv)
+{
+    json_engine *engine = new json_engine();
+    engine->json_load(argv[1], json_decode_callback);
+    printf("Usage: test\n");
+} 
