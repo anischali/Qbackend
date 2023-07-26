e0bb49b1c1358022d2f62c17a38d498d4894b2c4
Anis CHALI <anis.chali@exfo.com>
2023-07-25 21:04:59 -0400

    storage: add a storage engine

---
+++ src/storage/storage_engine.hpp
@@ -0,0 +1,19 @@
+#ifndef __STORAGE_ENGINE_H__
+#define __STORAGE_ENGINE_H__
+#include <string>
+
+class storage_engine {
+
+public:
+    storage_engine();
+    ~storage_engine();
+
+    static bool file_exist(std::string filename);
+    static bool path_exist(std::string path);
+    
+    static void create_directory(std::string path, bool recursive);
+    static void create_file(std::string filename, int mode);
+};
+
+
+#endif /* __STORAGE_ENGINE_H__ */
