ebaca625689a562cabeef2b546c2625d9b8e18f8
Anis CHALI <anis.chali1@outlook.com>
2024-11-17 06:57:40 -0500

    net: add an stun client api

---
+++ src/net/stun_client.cpp
@@ -0,0 +1,134 @@
+#include "stun_client.hpp"
+#include <errno.h>
+
+#define err_ret(msg, err) \
+    printf("%d: %s\n", err, msg); \
+    return err
+
+#define min(x, y) x < y ? x : y
+
+stun_client::stun_client(short port) : 
+    port{port}, ext_ip{0}
+{
+    fd = socket(AF_INET, SOCK_DGRAM, 0);
+}
+
+stun_client::~stun_client()
+{
+    close(fd);
+}
+
+int stun_client::stun_request(struct sockaddr_in stun_server) {
+    struct sockaddr_in laddr;  
+    struct stun_request_t request;
+    struct stun_response_t response;
+    int ret, len, i;
+    struct timeval tv;
+    uint8_t *attrs;
+    uint16_t attr_len = 0, attr_type = 0;
+
+    bzero(&laddr, sizeof(laddr));
+    laddr.sin_family = AF_INET;
+    laddr.sin_port = htons(port);
+
+    if (fd < 0) {
+        err_ret("Invalid socket fd", fd);
+    }
+    ret = bind(fd, (struct sockaddr *)&laddr, sizeof(laddr));
+    if (ret < 0) {
+        err_ret("Failed to bind", ret);
+    }
+    
+    ret = sendto(fd, &request, sizeof(request), 0, (struct sockaddr *)&stun_server, sizeof(stun_server));
+    if (ret < 0) {
+        err_ret("Failed to send data", ret);
+    }
+
+    tv.tv_sec = 5;
+    setsockopt(fd, SOL_SOCKET, SO_RCVTIMEO, (char*)&tv, sizeof(tv));
+    ret = recvfrom(fd, &response, sizeof(response), 0, NULL, 0);
+    if (ret < 0) {
+        err_ret("Failed to recv data", ret);
+    }
+
+    if (response.magic_cookie != request.magic_cookie)
+        return -EINVAL;
+
+    if (response.transaction_id != request.transaction_id)
+        return -EINVAL;
+
+    if (response.msg_type != htons(0x0101))
+        return -EINVAL;
+    
+    attrs = response.attributes;
+    len = min(response.msg_len, sizeof(response.attributes));
+
+    for (i = 0; i < len; i += (4 + attr_len)) {
+        attr_type = htons(*(int16_t*)(&attrs[i]));
+        attr_len = htons(*(int16_t*)(&attrs[i + 2]));
+        printf("%d\n", i);
+        if (attr_type == 0x020) {
+            ext_ip.sin_port = ntohs(*(int16_t*)(&attrs[i + 6]));
+            ext_ip.sin_port ^= (request.magic_cookie  >> 16);
+
+            ext_ip.sin_addr.s_addr = (attrs[i + 8] ^ ((request.magic_cookie & 0xff000000) >> 24) | 
+                            attrs[i + 9] ^ ((request.magic_cookie & 0x00ff0000) >> 16) |
+                            attrs[i + 10] ^ ((request.magic_cookie & 0x0000ff00) >> 8) |
+                            attrs[i + 11] ^ ((request.magic_cookie & 0x000000ff) >> 0));
+
+            return 0;
+        }
+    }
+
+    return -ENOENT;
+}
+
+
+int stun_client::stun_request(const char *stun_hostname, short stun_port) {
+    struct sockaddr_in stun_server, *addr;
+    struct addrinfo hints, *servinfo, *p;
+    struct sockaddr_in* h;
+    char *hostname, *service, hst[512];
+    int ret;
+    
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+
+    memset(hst, 0, sizeof(hst));
+    memcpy(hst, stun_hostname, min(512, strlen(stun_hostname)));
+    service = strtok_r(hst, ":/", &hostname);
+
+    if (hostname[0] == '/' && hostname[1] == '/')
+        hostname = &hostname[2];
+
+    ret = getaddrinfo(hostname, service, &hints, &servinfo);
+    if (ret != 0) {
+        return ret;
+    }
+    
+    for (p = servinfo; p != NULL; p = p->ai_next) {
+        addr = (struct sockaddr_in*)p->ai_addr;
+        memcpy(&stun_server, addr, sizeof(stun_server));
+    }
+    
+    freeaddrinfo(servinfo);
+
+    stun_server.sin_port = htons(port);
+    stun_server.sin_family = AF_INET;
+
+    return stun_request(stun_server);
+}
+
+/*
+int main() {
+    stun_client stun(55625);
+
+    int ret = stun.stun_request("stun:stun.l.google.com", 19302);
+
+    printf("ret: %d ip addr: %s port: %d\n", ret, inet_ntoa(stun.ext_ip.sin_addr), stun.ext_ip.sin_port);
+
+    stun.~stun_client();
+
+    return 0;
+}*/
---
+++ src/net/stun_client.hpp
@@ -0,0 +1,51 @@
+#ifndef __STUN_CLIENT_HPP__
+#define __STUN_CLIENT_HPP__
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <string.h>
+
+
+struct __attribute__((packed)) stun_request_t {
+    stun_request_t() {
+        for (int i = 0; i < sizeof(transaction_id); ++i) {
+            transaction_id[i] = rand() % 256;
+        }
+    }
+
+    const uint16_t msg_type = htons(0x1);
+    const uint16_t msg_len = htons(0x0);
+    const uint32_t magic_cookie = htonl(0x2112A442);
+    uint8_t transaction_id[12];
+};
+
+
+struct __attribute__((packed)) stun_response_t {
+    uint16_t msg_type = htons(0x1);
+    uint16_t msg_len = htons(0x0);
+    uint32_t magic_cookie = htonl(0x2112A442);
+    uint8_t transaction_id[12];
+    uint8_t attributes[1000];
+};
+
+class stun_client
+{
+private:
+    int fd;
+    short port;
+
+public:
+    struct sockaddr_in ext_ip;
+    stun_client(short port);
+    ~stun_client();
+
+    int stun_request(struct sockaddr_in stun_server);
+    int stun_request(const char *stun_hostname, short stun_port);
+
+};
+
+#endif
